bfcb55ba9ce17399e3ecd0785d1d820962bf9e5e
stephen <stephen@emmsdorf.com>
2019-10-02 20:00:24 -0500

    Initial commit - ASP.Net MVC Core application.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Magic8Ball.Net.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
