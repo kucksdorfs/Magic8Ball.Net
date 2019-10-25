a882509930590770a8bf5ffed4a90d5f3aba7623
Stephen <kucksdofs@gmail.com>
2019-10-25 07:21:52 -0500

    Do not override the previous question.

--- wwwroot/js/index.js
+++ wwwroot/js/index.js
@@ -8,20 +8,29 @@
             e.stopPropagation();
             e.preventDefault();
             let text = document.querySelector("input#txtYourQuestion");
+            if (!text.value) {
+                alert("Please provide a question.");
+                return;
+            }
 
             var ajax = new SK.AJAX({
                 url: "/answer?q=" + encodeURIComponent(text.value),
                 success: function (result, xhr) {
                     let questionResponse = document.querySelector("div#questionResponse"),
+                        currentResponse = document.createElement("div"),
                         responseValue = Magic8Ball.Responses[result.JSON];
 
-                    questionResponse.setAttribute("class", "");
                     if (!responseValue) {
-                        questionResponse.innerText = "Unknown response from the magic 8 ball.";
-                        return;
+                        currentResponse.innerHTML = text.value + " <br/> - " + "Unknown response from the magic 8 ball.";
+                    } else {
+                        currentResponse.innerHTML = text.value + " <br/> - " + responseValue.text;
+                        currentResponse.setAttribute("class", responseValue.responseType);
                     }
-                    questionResponse.innerText = responseValue.text;
-                    questionResponse.setAttribute("class", responseValue.responseType);
+
+                    questionResponse.prepend(currentResponse);
+                },
+                finally: function () {
+                    text.value = "";
                 }
             });
 
